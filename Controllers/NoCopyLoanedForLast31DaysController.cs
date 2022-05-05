1e5dc97932fb1826c46e7e3a55f65e7886fd75ba
Pawan Shrestha <pawanshresthait@gmail.com>
2022-05-05 22:21:54 +0545

    Number 12 feature complete

---
+++ Controllers/NoCopyLoanedForLast31DaysController.cs
@@ -0,0 +1,44 @@
+﻿using groupCW.Data;
+using groupCW.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace groupCW.Controllers
+{
+    public class NoCopyLoanedForLast31DaysController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public NoCopyLoanedForLast31DaysController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<NoCopyLoanedForLast31DaysViewModel> dvdList = _db.Loans
+                .Join(
+                    _db.DVDCopies,
+                    loans => loans.CopyNumber, dvdcopies => dvdcopies.CopyNumber,
+                    (loans, dvdcopies) => new NoCopyLoanedForLast31DaysViewModel
+                    {
+                        dvdNumber = dvdcopies.DVDNumber,
+                        dateOut = loans.DateOut
+                    }
+                )
+                .Join(
+                    _db.DVDTitles,
+                    dvdcopies => dvdcopies.dvdNumber, dvdtitle => dvdtitle.DVDNumber,
+                    (dvdcopies, dvdtitle) => new NoCopyLoanedForLast31DaysViewModel
+                    {
+                        dvdNumber = dvdcopies.dvdNumber,
+                        dvdTitle = dvdtitle.DVDTitles,
+                        dateOut = dvdcopies.dateOut
+                    }
+                )
+                .Where(
+                    x => (DateTime.Now.AddDays(-31) >= x.dateOut)
+                )
+                .ToList();
+
+            return View(dvdList);
+        }
+    }
+}
---
+++ ViewModel/NoCopyLoanedForLast31DaysViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace groupCW.ViewModel
+{
+    public class NoCopyLoanedForLast31DaysViewModel
+    {
+        public int? dvdNumber { get; set; }
+        public string? dvdTitle { get; set; }
+
+        public DateTime? dateOut { get; set; }
+    }
+}
